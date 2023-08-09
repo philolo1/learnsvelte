757c73bd6cfc437b097c118237434d8a4fe29b21
Patrick Klitzke <philologos14@gmail.com>
2023-08-09 17:30:43 +0900

    initial header with css

---
+++ svelteflix/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav
+	class="flex w-full h-12 bg-yellow-50 items-center justify-between max-w-col py-0 px-col text-accent"
+>
+	<a href="/"> SvelteFlix</a>
+
+	<span class="bg-green-50 h-20 w-40" />
+
+	<div>
+		<a href="/search">Search</a>
+		<a href="/watchlist">Watchlist</a>
+		<a href="/login">Log in</a>
+	</div>
+</nav>
+<slot />
---
+++ svelteflix/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        accent: 'hsl(1, 59%, 50%)',
+      },
+      padding: {
+       'col' : 'var(--col)',
+      },
+      maxWidth: {
+       'col' : 'var(--col)',
+      }
+    },
+  },
+  plugins: [],
+}
+
